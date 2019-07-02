505fa0c7dd34f866aacbc5d38248a4cc0939ce5a
Ryan <supermarioryan@gmail.com>
2019-07-02 20:53:47 +0100

    Attempt to get docs working in more browsers

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,5 +12,35 @@ module.exports = {
         link: 'https://github.com/rymate1234/tab-complete-input-vue'
       }
     ]
+  },
+  chainWebpack: (config, isServer) => {
+    // TODO: https://github.com/vuejs/vuepress/issues/1623 - Support IE 10-11
+    //
+
+    // This is for @vue/babel-preset-app, or it will 'forget' to inject
+    // the most primitive polyfills required by Vue (IE 10/11 support).
+    //
+    process.env.VUE_CLI_ENTRY_FILES = JSON.stringify(config.entry('app').values());
+
+    // Get existing js rule exclude function
+    //
+    let existingExcludes = config.module.rule('js').exclude.values();
+
+    // Add rule to always transpile auto-generated sources in .temp
+    //
+    config.module.rule('js').exclude.clear().add(filePath => {
+      const tempPath = config.resolve.alias.get('@temp');
+      const clientPath = config.resolve.alias.get('@client');
+      if (filePath.startsWith(tempPath) || filePath.startsWith(clientPath)) {
+        return false;
+      } else {
+        for (let i = 0; i < existingExcludes.length; i++) {
+          let returnedValue = existingExcludes[i](filePath);
+          if (returnedValue !== undefined)
+            return returnedValue;
+        }
+      }
+    });
+
   }
 }

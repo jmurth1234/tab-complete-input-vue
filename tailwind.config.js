896f39679718fac8eee837b9d6e25f01da4e5639
Ryan <supermarioryan@gmail.com>
2020-10-03 07:05:42 +0100

    Enable css purging

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  plugins: [require("@tailwindcss/ui")]
+  plugins: [require("@tailwindcss/ui")],
+  purge: [
+    "./examnple-src/**/*.html",
+    "./examnple-src/**/*.vue",
+    "./examnple-src/**/*.jsx"
+  ]
 };

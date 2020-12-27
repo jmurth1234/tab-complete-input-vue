4582163305a2c1d8753f409a6ad69fd796780688
Ryan Murthick <ryan.murthick@gmail.com>
2020-12-27 20:45:33 +0000

    Pre-render pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 module.exports = {
   lintOnSave: false,
-  runtimeCompiler: true
+  runtimeCompiler: true,
+
+  pluginOptions: {
+    prerenderSpa: {
+      registry: undefined,
+      renderRoutes: [
+        '/',
+        '/docs'
+      ],
+      useRenderEvent: false,
+      headless: true,
+      onlyProduction: true
+    }
+  }
 };

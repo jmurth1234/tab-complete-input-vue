0f7bdf5888316385edc874e855ec4bca1c30c048
Ryan Murthick <ryan.murthick@gmail.com>
2020-12-28 07:32:34 +0000

    > analyze bundle

--- example-src/components/fake-names.ts
+++ example-src/components/fake-names.ts
@@ -1,4 +1,4 @@
-import faker from "faker";
+import { name } from "faker";
 
 export const generate = (num: number): string[] =>
-  new Array<string>(num).fill("").map(() => faker.name.firstName());
+  new Array<string>(num).fill("").map(() => name.firstName());
